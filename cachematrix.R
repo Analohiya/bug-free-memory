69ae0ecbb76f3f90177057a1d143526361568002
Analohiya <82146978+Analohiya@users.noreply.github.com>
2021-04-08 17:52:10 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+	## We know that the functions "makeCacheMatrix" and "cacheSolve" 	## can cache its inverse.
 ## Write a short comment describing this function
-
+	## "makeCacheMatrix" creates a speciat matrix object that caches 	## its inverse while "cacheSolve" computes the inverse of the 		## special matrix returned.
 makeCacheMatrix <- function(x = matrix()) {
-j<-NULL
-set<-function(y){
-	x<<-y
-	j<<-NULL
+j <-NULL
+set <-function(y) {
+	x <<- y
+	j <<- NULL
 }
-get<-function()x
-setInverse<-function(inverse)j<<-inverse
-getInverse<-function()j
-list(set = set,get = get,
-setInverse = setInverse,
-getInverse = getInverse)
+	get <- function()x
+	setInverse <- function(inverse) j <<-inverse
+	getInverse <-function() j
+	list(set = set,get = get,
+	setInverse = setInverse,
+	getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+	## If the inverse of the function "cacheSolve" has already been 	## calculated and the matrix has not changed, then it retrieves 	## the inverse from the cache.
+	
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        j<-x$getInverse()
-        if(!s.null(j)){
-        	message("getting cached data")
+        j <- x$getInverse()
+        if(!s.null(j)) {
+        	message("getting cached information")
         	return(j)
         }
-        mat<-x$get()
-        j<-solve(mat,...)
+        data <- x$get()
+        j <- solve(data,...)
         x$setInverse(j)
         j
 }
